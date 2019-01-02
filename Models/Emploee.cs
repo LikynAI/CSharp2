c1529391bc659648675718caa29cda769d985f1e
LikynAI <Likyn2001@gmail.com>
2019-01-02 21:01:46 +0300

    Добавил веб-сервис часть

---
+++ Models/Emploee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication1.Models
+{
+	/// <summary>
+	/// Класс описывающий работника
+	/// </summary>
+	public class Emploee
+	{
+		public string name { get; set; }
+		public int id { get; set; }
+		public int DepId { get; set; }
+
+		public Emploee(string name, int id, int DepId)
+		{
+			this.name = name;
+			this.id = id;
+			this.DepId = DepId;
+		}
+
+		public Emploee() { }
+
+		public override string ToString()
+		{
+			return name;
+		}
+	}
+}
